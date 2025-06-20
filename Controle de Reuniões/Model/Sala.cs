f3ea300ff16896bed71e1f4918fe0ac8411240ad
Thiago Peixoto <thiago.peixots@gmail.com>
2025-06-20 01:38:49 -0300

    Init

---
+++ Controle de Reuniões/Model/Sala.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Controle_de_Reuniões.Model
+{
+    public class Sala
+    {
+
+        private string _id { get; set; }
+        private string _nome { get; set; }
+        private int  _numeroDeLugares { get; set; }
+        private string _setor { get; set; }
+
+        [DataType(DataType.Date)]
+        private DateTime _horariosDisponiveis;
+
+        public Sala(string id, string nome, int numeroDeLugares, string setor, DateTime horariosDisponiveis)
+        {
+
+            _id = id;
+            _nome = nome;
+            _numeroDeLugares = numeroDeLugares;
+            _setor = setor;
+
+            _horariosDisponiveis = horariosDisponiveis;
+        }
+
+    }
+}
---
+++ Controle de Reuniões/Model/Setores.cs
@@ -0,0 +1,13 @@
+﻿namespace Controle_de_Reuniões.Model
+{
+    public enum Setores
+    {
+        CTM,
+        Financeiro,
+        Inova,
+        Segurança_e_Meio_Ambiente,
+        GG,
+        GP,
+
+    }
+}
